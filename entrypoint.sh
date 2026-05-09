a2c28329fc37b2c2d742585f9f762d43acd58a50
Gyztor Mizirath <44737827+Gyztor@users.noreply.github.com>
2026-05-08 19:24:40 -0500

    steam cmd windows platform is appearntly needed
    thought they fixed this but i guess not. might look into alternatives on how to fix this with depot downloader or something.

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,7 +62,7 @@ if [ ${LEGACY_INSTALL} = "FALSE" ]; then
 	fi
 fi
 
-./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASSWORD} ${STEAM_AUTH} +force_install_dir ${INSTALL_DIRECTORY} +app_license_request 2519830 +app_update 2519830 -beta ${STEAM_BRANCH} ${STEAMCMD_BETA_PASSWORD} validate +quit
+./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASSWORD} ${STEAM_AUTH} +@sSteamCmdForcePlatformType windows +force_install_dir ${INSTALL_DIRECTORY} +app_license_request 2519830 +app_update 2519830 -beta ${STEAM_BRANCH} ${STEAMCMD_BETA_PASSWORD} validate +quit
 
 # Modding stuff
 HEADLESS_DIRECTORY="/home/container"
